d96d97d59fb9e506a2174eb4a1319c01b8023fb7
Ирина Некрасова <iwannahaveaparrot@yandex.ru>
2023-10-20 01:21:43 +0300

    Complited task

--- anfisa_for_friends/test.py
+++ anfisa_for_friends/test.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 ice_cream_catalog = [
     {
         'id': 0,
@@ -20,14 +18,5 @@ ice_cream_catalog = [
     },
 ]
 
-
-def ice_cream_detail(request, pk):
-    template = 'ice_cream/detail.html'
-    context = {'sort_ice_cream': ice_cream_catalog[pk]}
-    return render(request, template, context)
-
-
-def ice_cream_list(request):
-    template = 'ice_cream/list.html'
     context = {'ice_cream_list': ice_cream_catalog}
-    return render(request, template, context)
+    print (context)
